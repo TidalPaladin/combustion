8874717379a77e83d3cd1b894bedd4663d5de99c
Scott Chase Waggener <tidalpaladin@protonmail.com>
2020-04-12 22:10:42 -0500

    Fixed combustion.module imports

--- src/combustion/modules/__init__.py
+++ src/combustion/modules/__init__.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from .bottleneck import Bottleneck1d, Bottleneck2d, Bottleneck3d, BottleneckFactorized2d, BottleneckFactorized3d
+from .conv import DownSample3d, UpSample2d, UpSample3d
+from .factorized import Conv1d, Conv2d, Conv3d, ConvTranspose1d, ConvTranspose2d, ConvTranspose3d
+
+
+__all__ = [
+    "Conv3d",
+    "ConvTranspose3d",
+    "Conv2d",
+    "ConvTranspose2d",
+    "Conv1d",
+    "ConvTranspose1d",
+    "DownSample3d",
+    "UpSample3d",
+    "UpSample2d",
+    "Bottleneck3d",
+    "Bottleneck2d",
+    "Bottleneck1d",
+    "BottleneckFactorized3d",
+    "BottleneckFactorized2d",
+]
