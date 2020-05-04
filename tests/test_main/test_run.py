cc41f507c6be37e8e8cfbbe020c5f1c2915c9171
Scott Chase Waggener <tidalpaladin@protonmail.com>
2020-05-04 13:37:16 -0500

    Separate template and library features

--- tests/test_main/test_run.py
+++ tests/test_main/test_run.py
@@ -7,9 +7,9 @@ import sys
 
 def test_fast_dev_run():
     sys.argv = [sys.argv[0], "trainer=test"]
-    runpy.run_module("src.project", run_name="__main__", alter_sys=True)
+    runpy.run_module("examples.basic", run_name="__main__", alter_sys=True)
 
 
 def test_dev_run():
     sys.argv = [sys.argv[0], "trainer=test", "trainer.params.fast_dev_run=False"]
-    runpy.run_module("src.project", run_name="__main__", alter_sys=True)
+    runpy.run_module("examples.basic", run_name="__main__", alter_sys=True)
