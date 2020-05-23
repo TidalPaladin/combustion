6902d4a75be4df4e67039dd96d3f81ef52d6165f
Scott Chase Waggener <tidalpaladin@protonmail.com>
2020-05-23 11:06:21 -0500

    Added cuda_or_skip decorator

--- src/combustion/testing/__init__.py
+++ src/combustion/testing/__init__.py
@@ -10,6 +10,7 @@ from .assertions import (
     assert_zero_grad,
 )
 from .lightning import LightningModuleTest
+from .utils import cuda_or_skip
 
 
 __all__ = [
@@ -19,5 +20,6 @@ __all__ = [
     "assert_in_training_mode",
     "assert_in_eval_mode",
     "assert_tensors_close",
+    "cuda_or_skip",
     "LightningModuleTest",
 ]
