da18474167e431c923e53e7e093219aebea9bb0c
Scott Chase Waggener <tidalpaladin@protonmail.com>
2020-07-08 13:35:06 -0500

    Ensure failed main tests are detected

--- examples/basic/__main__.py
+++ examples/basic/__main__.py
@@ -78,3 +78,4 @@ def main(cfg):
 
 if __name__ == "__main__":
     main()
+    combustion.check_exceptions()
--- tests/test_main/test_run.py
+++ tests/test_main/test_run.py
@@ -7,8 +7,10 @@ import sys
 
 import pytest
 
+from combustion import MultiRunError
 
-@pytest.fixture(autouse=True)
+
+@pytest.fixture(autouse=False)
 def set_caplog(caplog):
     caplog.set_level(logging.CRITICAL, logger="__main__")
     caplog.set_level(logging.CRITICAL, logger="combustion")
@@ -27,7 +29,8 @@ def test_multirun():
 
 def test_multirun_handles_exception():
     sys.argv = [sys.argv[0], "-m", "trainer=test", "model.params.batch_size=-1, 8"]
-    runpy.run_module("examples.basic", run_name="__main__", alter_sys=True)
+    with pytest.raises(MultiRunError):
+        runpy.run_module("examples.basic", run_name="__main__", alter_sys=True)
 
 
 @pytest.mark.parametrize("ex", [KeyboardInterrupt, SystemExit])
