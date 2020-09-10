6f9c109eca12f5ce73a034fb29a0b7d219465e24
Scott Chase Waggener <tidalpaladin@protonmail.com>
2020-09-10 10:35:03 -0500

    Add norm_min/norm_max args to ClampAndNormalize

--- src/combustion/nn/modules/clamp_normalize.py
+++ src/combustion/nn/modules/clamp_normalize.py
@@ -6,7 +6,7 @@ from torch import Tensor
 
 
 class ClampAndNormalize(nn.Module):
-    r"""Clamps an input tensor and normalizes the clamped result to the range :math:`[0, 1]`.
+    r"""Clamps an input tensor and normalizes the clamped result to a fixed range.
 
     Args:
 
@@ -15,23 +15,46 @@ class ClampAndNormalize(nn.Module):
 
         maximum (float):
             The upper bound for clamping
+
+        norm_min (float):
+            The lower bound of the normalized range
+
+        norm_max (float):
+            The upper bound of the normalized range
     """
 
-    def __init__(self, minimum: float, maximum: float):
+    def __init__(self, minimum: float, maximum: float, norm_min: float = 0.0, norm_max: float = 1.0):
         super().__init__()
         minimum = float(minimum)
         maximum = float(maximum)
         if minimum >= maximum:
             raise ValueError(f"Expected minimum < maximum but found {minimum}, {maximum}")
+
+        norm_min = float(norm_min)
+        norm_max = float(norm_max)
+        if norm_min >= norm_max:
+            raise ValueError(f"Expected norm_min < norm_max but found {norm_min}, {norm_max}")
+
         self._minimum = minimum
         self._maximum = maximum
+        self._norm_min = norm_min
+        self._norm_max = norm_max
         self._delta = maximum - minimum
+        self._output_delta = norm_max - norm_min
 
     def extra_repr(self):
         s = f"min={self._minimum}, max={self._maximum}"
+        if self._norm_min != 0:
+            s += f", norm_min={self._norm_min}"
+        if self._norm_max != 1:
+            s += f", norm_max={self._norm_max}"
         return s
 
     def forward(self, inputs: Tensor) -> Tensor:
         inputs = inputs.float()
         outputs = inputs.clamp(self._minimum, self._maximum).sub_(self._minimum).div_(self._delta)
+
+        if self._norm_min != 0 or self._norm_max != 1:
+            outputs = outputs.mul_(self._output_delta).add_(self._norm_min)
+
         return outputs
--- tests/test_combustion/test_nn/test_module/test_clamp_normalize.py
+++ tests/test_combustion/test_nn/test_module/test_clamp_normalize.py
@@ -18,19 +18,23 @@ class TestClampAndNormalize(TorchScriptTestMixin, TorchScriptTraceTestMixin):
         return torch.tensor([10])
 
     @pytest.mark.parametrize(
-        "inputs,min,max,expected",
+        "inputs,min,max,norm_min,norm_max,expected",
         [
-            pytest.param(3, -10, 10, 0.65),
-            pytest.param(0, -10, 10, 0.5),
-            pytest.param(2, 0, 10, 0.2),
-            pytest.param(0, -10, 0, 1.0),
+            pytest.param(3, -10, 10, 0, 1, 0.65),
+            pytest.param(0, -10, 10, 0, 1, 0.5),
+            pytest.param(2, 0, 10, 0, 1, 0.2),
+            pytest.param(0, -10, 0, 0, 1, 1.0),
+            pytest.param(0, -10, 0, -1, 1, 1.0),
+            pytest.param(-10, -10, 0, -1, 1, -1.0),
+            pytest.param(-10, -10, 0, -1, 0, -1.0),
+            pytest.param(0, -10, 0, -1, 0, 0.0),
         ],
     )
-    def test_result(self, inputs, min, max, expected):
+    def test_result(self, inputs, min, max, norm_min, norm_max, expected):
         inputs = torch.tensor([inputs])
         inputs_copy = inputs.clone()
         expected = torch.tensor([expected])
-        layer = ClampAndNormalize(min, max)
+        layer = ClampAndNormalize(min, max, norm_min, norm_max)
         actual = layer(inputs)
         assert torch.allclose(expected, actual)
         assert torch.allclose(inputs, inputs_copy)
@@ -40,5 +44,5 @@ class TestClampAndNormalize(TorchScriptTestMixin, TorchScriptTraceTestMixin):
             ClampAndNormalize(10, 10)
 
     def test_repr(self):
-        layer = ClampAndNormalize(-10, 10)
+        layer = ClampAndNormalize(-10, 10, -1, 2)
         print(str(layer))
