1051b5a1977e07916f0f71da59d5154febb2c340
Scott Chase Waggener <tidalpaladin@protonmail.com>
2020-09-13 14:44:23 -0500

    Replace unsqueeze with view

--- src/combustion/nn/modules/dropconnect.py
+++ src/combustion/nn/modules/dropconnect.py
@@ -8,11 +8,29 @@ from torch import Tensor
 
 class DropConnect(nn.Module):
     r"""Implements DropConnect as defined in `Regularization of Neural Networks using DropConnect`_
-    for use with convolutional layers.
+    for use with convolutional layers. This layer is intended to be applied to convolutional layers
+    with an accompanying residual connection. DropConnect will zero the output of the main pathway
+    with some probability, relying on the skip connection to propagate information.
+
+    Like other dropout layers, DropConnect attempts to force layers to extract features in a way that
+    is not dependent on previous layers.
 
     Args:
         ratio (float):
             The ratio of elements to be dropped
+
+    Example::
+
+        >>> main_path = nn.Conv2d(1, 1, 3, padding=1)
+        >>> drop_connect = DropConnect(0.2)
+        >>>
+        >>> main_output = main_path(inputs)
+        >>> # zero drop_connect_output with p=0.2
+        >>> drop_connect_output = drop_connect(main_output)
+        >>>
+        >>> # when drop_connect_output is zeroed, only residual_output remains
+        >>> residual_output = inputs
+        >>> final_output = drop_connect_output + residual_output
 
     Shape
         * Input: :math:`(N, C, d_1 \dots d_n)` where :math:`d_1 \dots d_n` is any number of
@@ -34,6 +52,8 @@ class DropConnect(nn.Module):
 
         batch_size = x.shape[0]
         mask = self.ratio + torch.rand(batch_size).type_as(x).floor_()
-        for i in range(x.ndim - 2):
-            mask = mask.unsqueeze(-1)
+        target_shape = (batch_size,) + (1,) * (x.ndim - 1)
+        mask = mask.view(target_shape)
+        assert mask.ndim == x.ndim
+        assert mask.shape[0] == batch_size
         return x / self.ratio * mask
--- tests/test_combustion/test_nn/test_module/test_dropconnect.py
+++ tests/test_combustion/test_nn/test_module/test_dropconnect.py
@@ -9,20 +9,39 @@ from combustion.testing import TorchScriptTestMixin, TorchScriptTraceTestMixin
 
 
 class TestDropConnect(TorchScriptTestMixin, TorchScriptTraceTestMixin):
+    @pytest.fixture(params=["train", "eval"])
+    def mode(self, request):
+        return request.param
+
+    @pytest.fixture(params=[1, 4])
+    def batch_size(self, request):
+        return request.param
+
     @pytest.fixture
-    def model(self):
-        return DropConnect(0.3)
+    def model(self, mode):
+        layer = DropConnect(0.3)
+        if mode == "train":
+            layer.train()
+        else:
+            layer.eval()
+        return layer
 
     @pytest.fixture(
         params=[pytest.param(1, id="1d"), pytest.param(2, id="2d"), pytest.param(3, id="3d"),]
     )
-    def data(self, request):
+    def data(self, request, batch_size):
         torch.random.manual_seed(42)
-        return torch.rand(1, 4, *([32] * request.param), requires_grad=True)
+        return torch.rand(batch_size, 4, *([32] * request.param), requires_grad=True)
 
-    def test_forward(self, model, data):
+    def test_forward(self, mode, model, data):
         output = model(data)
-        assert output.shape == data.shape
+        if mode == "train":
+            assert model.training
+            assert output.shape == data.shape
+            assert output is not data
+        else:
+            assert not model.training
+            assert output is data
 
     def test_backward(self, model, data):
         output = model(data)
