Fix scaling w/ repeated from_predefined calls

src/combustion/models/efficientdet.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from copy import deepcopy
 from typing import List, Optional
 
 import torch.nn as nn
@@ -56,24 +55,23 @@ class _EfficientDet(_EfficientNet):
             block_configs, width_coeff, depth_coeff, width_divisor, min_width, stem, head
         )
         self.fpn_levels = fpn_levels
-        block_configs = deepcopy(block_configs)
 
         # convolutions mapping backbone feature maps to constant number of channels
         fpn_convs = []
         output_filters = self.round_filters(fpn_filters, 1.0, width_divisor, min_width)
         self.__fpn_filters = output_filters
-        for i, config in enumerate(block_configs):
+        for i, config in enumerate(self.block_configs):
             if i + 1 in fpn_levels:
                 input_filters = config.output_filters
                 conv = self.Conv(input_filters, output_filters, kernel_size=1)
                 fpn_convs.append(conv)
 
         for i in fpn_levels:
-            if i == len(block_configs) + 1:
-                input_filters = block_configs[-1].output_filters
+            if i == len(self.block_configs) + 1:
+                input_filters = self.block_configs[-1].output_filters
                 conv = self.Conv(input_filters, output_filters, kernel_size=3, stride=2, padding=1)
                 fpn_convs.append(conv)
-            elif i > len(block_configs) + 1:
+            elif i > len(self.block_configs) + 1:
                 input_filters = output_filters
                 conv = self.Conv(input_filters, output_filters, kernel_size=3, stride=2, padding=1)
                 fpn_convs.append(nn.Sequential(nn.ReLU(), conv))
@@ -196,7 +194,9 @@ class _EfficientDet(_EfficientNet):
             "fpn_levels": fpn_levels,
         }
         final_kwargs.update(kwargs)
-        return cls(**final_kwargs)
+        result = cls(**final_kwargs)
+        result.compound_coeff = compound_coeff
+        return result
 
 
 class EfficientDet1d(_EfficientDet, metaclass=_EfficientDetMeta):

src/combustion/models/efficientnet.py
@@ -138,14 +138,14 @@ class _EfficientNet(nn.Module):
         self.__depth_coeff = float(depth_coeff)
 
         output_filters = []
-        for config in block_configs:
+        for config in self.block_configs:
             # update config according to scale coefficients
             config.input_filters = self.round_filters(config.input_filters, width_coeff, width_divisor, min_width)
             config.output_filters = self.round_filters(config.output_filters, width_coeff, width_divisor, min_width)
             config.num_repeats = self.round_repeats(depth_coeff, config.num_repeats)
             output_filters.append(config.output_filters)
 
-        self.__input_filters = block_configs[0].input_filters
+        self.__input_filters = self.block_configs[0].input_filters
         self.__output_filters = tuple(output_filters)
 
         # Conv stem (default stem used if none given)
@@ -153,7 +153,7 @@ class _EfficientNet(nn.Module):
             self.stem = stem
         else:
             in_channels = 3
-            first_block = next(iter(block_configs))
+            first_block = next(iter(self.block_configs))
             output_filters = first_block.input_filters
             bn_momentum = first_block.bn_momentum
             bn_epsilon = first_block.bn_epsilon
@@ -165,7 +165,7 @@ class _EfficientNet(nn.Module):
 
         # MobileNetV3 convolution blocks
         blocks = []
-        for config in block_configs:
+        for config in self.block_configs:
             conv_block = self.__class__._get_blocks(config)
             blocks.append(conv_block)
         self.blocks = nn.ModuleList(blocks)
@@ -319,8 +319,8 @@ class _EfficientNet(nn.Module):
             https://arxiv.org/abs/1905.11946
         """
         # from paper
-        alpha = 1.1
-        beta = 1.0
+        alpha = 1.2
+        beta = 1.1
         width_divisor = 8.0
 
         depth_coeff = alpha ** compound_coeff