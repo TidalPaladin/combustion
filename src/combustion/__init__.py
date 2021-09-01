16aae79fe135ee3bdf1b262cf9b7570e71fd9e87
Scott Chase Waggener <tidalpaladin@protonmail.com>
2021-09-01 08:59:13 -0500

    Remove HydraMixin / __main__

--- src/combustion/__init__.py
+++ src/combustion/__init__.py
@@ -2,20 +2,10 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from .__main__ import MultiRunError, auto_lr_find, check_exceptions, clear_exceptions, initialize, main
 from .version import __version__
 
 
 logger = logging.getLogger(name="combustion")
 
 
-__all__ = [
-    "main",
-    "auto_lr_find",
-    "MultiRunError",
-    "check_exceptions",
-    "clear_exceptions",
-    "initialize",
-    "logger",
-    "__version__",
-]
+__all__ = ["logger", "__version__"]
